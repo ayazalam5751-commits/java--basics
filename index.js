8c0b1b1c635a588510f1b208d849dc023d103864
ayazalam5751-commits <ayazalam5751@gmail.com>
2026-09-12 00:30:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+//Arthmetic operation 
+// let a=5;
+// let b=2;
+
+// console.log("a = ",a," & b= ",b);
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a/b=",a/b);
+// console.log("a*b=",a*b);
+// console.log("a%b=",a%b);
+// console.log("a**b=",a**b);
+
+
+
+//uniary operator
+// let a=5;
+// let b=2;
+
+// console.log("a = ",a," & b= ",b);
+//a=a+1;//6
+//a++;//6
+
+//console.log("++a=",++a)//6
+// console.log("a++=",a++);
+// console.log(" a= " ,a);
+
+// console.log(" --a =",--a);
+// console.log(" a = " ,a);
+
+
+
+//Assignment operator
+// let a=5;
+// let b=2;
+
+//a=a+4=9
+//a -=4;//a=a-4=1
+//a/=4;//a=a/4=1.25
+//a%=4//a=a%4=1
+// a**=4//a=a**4=625
+// console.log("a = ", a)
+
+
+
+
+//Comparision operator
+// let a=5;
+// let b=5;
+
+//console.log("5==2",a==b);//false
+// console.log("a!=b",a!=b)//true
+//console.log("a==b",a==b)
+//console.log("a===b",a===b)//false
+// console.log("a!==b",a!==b)
+// console.log("a>4",a>4)//true
+// console.log("a<4",a<4)//false
+// console.log("a<=b",a<=b)//true
+// console.log("a>=b",a>=b)//true
+
+
+// logical operator

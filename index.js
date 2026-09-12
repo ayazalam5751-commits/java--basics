6f15c1978ac87b7c2becbb9b08e2df51da3ca874
MD AYAZ ALAM <ayazalam5751@gmail.com>
2026-09-12 22:37:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -175,11 +175,52 @@
 // }else{
 //     document.write("Not Ellible");
 // }
+// / let num = prompt("enter a number")
 
-let a = prompt("Enter first number:");
-let b = prompt("Enter second number:");
-let sum = a+b;
-document.write
+// if (num % 5 ===0 ){
+//     console.log(num," is a multiple of 5 ")
+// }else{
+//     console.log(num,"is not multiple of 5")
+// }
+
+let score = prompt("enter your score(0-100")
+let grade;
+
+if(score>=90 && score <=100){
+    grade = "A";
+}else if(score >=70 && score<=89){
+    grade="B"
+}else if (score>=60 && score<=69){
+    grade = "C"
+}else if (score>+50 && score <=59){
+    grade = "D"
+}
+console.log("according to your scores ,your grade was; " ,grade)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(5+5.8 == 10.8)
+// console.log(0.2+0.1 == 0.3)//false kyuki decimal point 0.3 infinite tk ja rha hai
+
+// console.log(0.3+0.2==0.5)
+// console.log(2+1 == 3)
 
 
 
